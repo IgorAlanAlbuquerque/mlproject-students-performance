1fb57ab21699753d5503b101460f44fcb27dfc1e
igor albuquerque <igoralbuquerque102@hotmail.com>
2024-12-19 19:25:04 -0300

    data transformation class completado

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,13 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -38,7 +41,3 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e, sys)
-        
-if __name__ == "__main__":
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
